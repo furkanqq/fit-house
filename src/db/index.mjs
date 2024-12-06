28bb2ea9d563714be4634c9cd029e898e957aeb9
furkanqq <133240374+furkanqq@users.noreply.github.com>
2024-12-06 10:39:20 +0300

    log:

--- src/db/index.mjs
+++ src/db/index.mjs
@@ -23,8 +23,9 @@
 
 // export default connectDB;
 
-
 import { Pool } from "pg";
+
+console.log(process.env.DATABASE_URL, "process.env.DATABASE_URL");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // .env dosyasından okur
