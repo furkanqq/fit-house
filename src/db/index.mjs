1c3832138ebb54fa69b191ec18372bb3e7c9af65
furkanqq <133240374+furkanqq@users.noreply.github.com>
2024-12-06 09:24:10 +0300

    Veritabanı dosyasının var olup olmadığını kontrol et

--- src/db/index.mjs
+++ src/db/index.mjs
@@ -1,11 +1,18 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "path";
+import fs from "fs";
 
 // SQLite bağlantısı
 const connectDB = async () => {
+  const dbPath = path.resolve(process.cwd(), "database.sqlite");
+
+  // Veritabanı dosyasının var olup olmadığını kontrol et
+  console.log("Database Path:", dbPath);
+  console.log("Database Exists:", fs.existsSync(dbPath));
+
   return open({
-    filename: path.resolve(process.cwd(), "database.sqlite"), // Mutlak yol
+    filename: dbPath,
     driver: sqlite3.Database,
   });
 };
