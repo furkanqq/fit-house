477f491d9567ac98ab124597a5dcf283218d3116
furkanqq <133240374+furkanqq@users.noreply.github.com>
2024-12-06 09:30:12 +0300

    dizin kopyalama /tmp

--- src/db/index.mjs
+++ src/db/index.mjs
@@ -2,17 +2,21 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "path";
 import fs from "fs";
+import os from "os";
 
-// SQLite bağlantısı
 const connectDB = async () => {
   const dbPath = path.resolve(process.cwd(), "database.sqlite");
+  const tempDbPath = path.join(os.tmpdir(), "database.sqlite");
 
-  // Veritabanı dosyasının var olup olmadığını kontrol et
-  console.log("Database Path:", dbPath);
-  console.log("Database Exists:", fs.existsSync(dbPath));
+  // Veritabanını geçici dizine kopyala
+  if (!fs.existsSync(tempDbPath)) {
+    fs.copyFileSync(dbPath, tempDbPath);
+    console.log("Database copied to:", tempDbPath);
+  }
 
+  // Veritabanına bağlan
   return open({
-    filename: dbPath,
+    filename: tempDbPath,
     driver: sqlite3.Database,
   });
 };
